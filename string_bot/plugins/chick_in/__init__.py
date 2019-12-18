326570aa32f9311ec501313d1e2a2f8b8d837857
jinserrr <46820840+jinserrr@users.noreply.github.com>
2019-12-19 02:45:30 +0800

    Changed to sqlite3

--- string_bot/plugins/chick_in/__init__.py
+++ string_bot/plugins/chick_in/__init__.py
@@ -2,12 +2,12 @@ from nonebot import CommandGroup, CommandSession
 from .chick_in_system import *
 from .check_in_image import ImageProcessing
 from .data_source import get_image
+from shutil import copyfile
 
 __plugin_name__ = '签到'
 __plugin_usage__ = r"""签到服务
 
 指令: 签到 / 注册"""
-
 
 cg = CommandGroup('chick_in', only_to_me=False)
 
@@ -15,8 +15,8 @@ cg = CommandGroup('chick_in', only_to_me=False)
 @cg.command('registered', aliases=['注册'])
 async def registered(session: CommandSession):
     user_id = session.ctx['sender']['user_id']
-    data = get_user_info()
-    if str(user_id) not in list(data.keys()):
+
+    if user_registration_interval_judgment(user_id):
         user_registration(session.ctx)
         await session.send('注册成功!')
     else:
@@ -27,13 +27,12 @@ async def registered(session: CommandSession):
 async def chick_in_cmd(session: CommandSession):
     try:
         uid = session.ctx['sender']['user_id']
-        uid = str(uid)
 
         if check_in_interval_judgment(uid):
 
             chick_in(uid)
 
-            # image = await get_image(uid)
+            image = await get_image(uid)
 
             card = session.ctx.get('sender').get('card')
             if card:
@@ -42,15 +41,17 @@ async def chick_in_cmd(session: CommandSession):
                 nickname = session.ctx['sender']['nickname']
                 text = chick_in_text(uid, nickname)
 
-            # image = ImageProcessing(image, text, 256, 'send')
-            # image.save()
+            image = ImageProcessing(image, text, 256, 'send')
+            image.save()
 
             # 不抛异常, 但是没有酷Q pro就无法发图
             # 无法直接捕获异常
             # air时用:
-            await session.send(text)
+            # await session.send(text)
             # pro时用:
-            # await session.send('[CQ:image,file=send.png]')
+            # 把这个文件复制到docker挂载的coolq的文件夹里才能识别到
+            copyfile('data/send.png', '/home/ubuntu/coolq-pro/data/send.png')
+            await session.send('[CQ:image,file=file:///data/send.png]')
         else:
             await session.send('您今天已经签到过了')
     except KeyError:
